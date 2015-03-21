a54747054aafa09e4a3994b153fcb119dc326629
Henric Andersson <ha@beaglebone.localdomain>
2015-03-21 23:46:34 +0000

    Improved IR error handling

--- ir.py
+++ ir.py
@@ -13,28 +13,38 @@ class IRToy (threading.Thread):
     self.daemon = True
 
   def init(self):
-    print "Initializing USB IR Toy interface"
+    print "INFO: Initializing USB IR Toy interface"
     
-    self.port = serial.Serial(self.serialport, baudrate=115200)
+    self.port = serial.Serial(self.serialport, baudrate=115200, rtscts=False, timeout=10)
     self.state = "unknown"
-    self.port.flushInput()
-    self.port.flushOutput()
+    return self.configure()
 
-    self.port.write("\x00\x00\x00\x00\x00")
-    time.sleep(0.5)
-    self.port.flushOutput()
-    self.port.write("S")
-    
-    result = self.port.read(3)
-    if result != "S01":
-      print "Failed to initialize USB IR Toy"
-      return False
-    # Make sure we get details when we transmit
-    self.port.write("\x24\x25\x26")
-    
-    print "Interface online"
-    self.start()
-    return True
+  def configure(self):
+    while True:
+      try:
+        print "INFO: Configuring USB IR Toy"
+
+        self.port.flushInput()
+        self.port.flushOutput()
+
+        self.write("\x00\x00\x00\x00\x00")
+        time.sleep(0.1)
+        self.write("S")
+        
+        result = self.read(3)
+        if result != "S01":
+          print "ERR: Failed to initialize USB IR Toy"
+          return False
+        # Make sure we get details when we transmit
+        self.write("\x24\x25\x26")
+        
+        print "INFO: Interface online"
+        self.start()
+        return True
+      except:
+        print "WARN: Configure failed, retrying"
+        time.sleep(1)
+
     
   def readIR(self):
     """Reads a complete IR command from the receiver and returns the buffer"""
@@ -44,7 +54,7 @@ class IRToy (threading.Thread):
     cmd = ""
     while not (p == '\xff' and b == '\xff'):
       p = b
-      b = self.port.read()
+      b = self.read()
       cmd += b
     return cmd
 
@@ -53,44 +63,49 @@ class IRToy (threading.Thread):
     
   def writeIR(self, cmd):
     """Sends a complete IR command which has previously been received with readIR()"""
-    self.port.write("\x03")
-
-    # Deal with any IR commands which haven't been read
-    bytes2send = ord(self.port.read(1))
-    while bytes2send != 62:
-      print "Need to flush old buffer (bytes2send = %d)" % bytes2send
-      p = b = bytes2send
-      while not (p == '\xff' and b == '\xff'):
-        p = b
-        b = self.port.read()
-      print "Flushed old IR command, lets try again"
-      bytes2send = ord(self.port.read(1))
-
-    tosend = len(cmd)
     while True:
-      if len(cmd) == 0:
-        break
-      elif bytes2send < len(cmd):
-        buffer = cmd[:bytes2send]
-        cmd = cmd[bytes2send:]
-      else:
-        buffer = cmd
-        cmd = ""
-      self.port.write(buffer)
-      bytes2send = ord(self.port.read(1))
-      
-    resp = self.port.read(1)
-    if resp != "t":
-      print "ERROR: Protocol error, aborting"
-      return False
-      
-    high = ord(self.port.read(1))
-    low = ord(self.port.read(1))
-    sent = high << 8 | low
-    if sent != tosend:
-      print "ERROR: Wanted to send %d bytes, sent %d" % (tosend, sent)
-    result = self.port.read(1)
-    return result == "C"
+      try: 
+        self.write("\x03")
+
+        # Deal with any IR commands which haven't been read
+        bytes2send = ord(self.read(1))
+        while bytes2send <> 62:
+          print "DBG: Need to flush old buffer (bytes2send = %d)" % bytes2send
+          p = b = bytes2send
+          while not (p == '\xff' and b == '\xff'):
+            p = b
+            b = self.read()
+          print "DBG: Flushed old IR command, lets try again"
+          bytes2send = ord(self.read(1))
+
+        tosend = len(cmd)
+        while True:
+          if len(cmd) == 0:
+            break
+          elif bytes2send < len(cmd):
+            buffer = cmd[:bytes2send]
+            cmd = cmd[bytes2send:]
+          else:
+            buffer = cmd
+            cmd = ""
+          self.write(buffer)
+          bytes2send = ord(self.read(1))
+          
+        resp = self.read(1)
+        if resp != "t":
+          print "ERR: Protocol error, aborting"
+          return False
+          
+        high = ord(self.read(1))
+        low = ord(self.read(1))
+        sent = high << 8 | low
+        if sent != tosend:
+          print "ERR: Wanted to send %d bytes, sent %d" % (tosend, sent)
+        result = self.read(1)
+        return result == "C"
+      except:
+        print "WARN: Timeout sending IR, retrying."
+        self.configure()
 
   def run(self):
     while True:
@@ -100,3 +115,23 @@ class IRToy (threading.Thread):
       else:
         print "WARN: IR command failed to send"
         
+  def read(self, count = 1):
+    data = self.port.read(count)
+    if len(data) <> count:
+      print "ERR: Requested %d bytes, got %d" % (count, len(data))
+      raise TimeoutException("Read Timeout")
+    return data
+
+  def write(self, data):
+    count = 0
+    #for byte in data:
+    #  count += self.port.write(byte)
+    count = self.port.write(data)
+    time.sleep(0.01)
+    if len(data) <> count:
+      print "ERR: Wanted to write %d bytes, wrote %d" % (len(data), count)
+      raise TimeoutException("Write Timeout")
+    return count
+
+class TimeoutException(Exception):
+  pass
--- tester.py
+++ tester.py
@@ -7,19 +7,29 @@ import sys
 import base64
 import json
 
-if len(sys.argv) < 4:
-  print "Usage: tester.py <serial port> <input file> <command>"
+if len(sys.argv) != 2 and len(sys.argv) != 4:
+  print "Usage: tester.py <input file> [<command> <serial port>]"
   exit(1)
 
-ir = IRToy(sys.argv[1])
-if not ir.init():
-  print "Unable to initialize IR Toy"
-  exit(1)
-
-jdata = open(sys.argv[2])
+jdata = open(sys.argv[1])
 data = json.load(jdata)
 
-if ir.writeIR(base64.urlsafe_b64decode(data[sys.argv[3]].encode("utf-8"))):
-  print "Command sent successfully"
+
+if len(sys.argv) == 2:
+	print "Available commands for device:"
+	for cmd in data:
+		print "  %s" % cmd
 else:
-  print "Was unable to send command"
+	if sys.argv[2] not in data:
+		print "ERROR: %s is not a recognized command" % sys.argv[2]
+		exit(1)
+
+	ir = IRToy(sys.argv[3])
+	if not ir.init():
+	  print "ERR: Unable to initialize IR Toy on %s" % sys.argv[3]
+	  exit(1)
+
+	if ir.writeIR(base64.urlsafe_b64decode(data[sys.argv[2]].encode("utf-8"))):
+		print "INFO: Command sent successfully"
+	else:
+		print "ERR: Was unable to send command"

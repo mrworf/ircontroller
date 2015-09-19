8822d1e93e2e13e6e95b3bc1a2efc7a84c81172b
Henric Andersson <henric@sensenet.nu>
2015-09-19 15:50:09 -0700

    Updated test tool
    - Now uses argparse()
    - Nicer to look at :)

--- tester.py
+++ tester.py
@@ -6,30 +6,37 @@ from ir_deluxe import IRInterface
 import sys
 import base64
 import json
+import argparse
 
-if len(sys.argv) != 2 and len(sys.argv) != 4:
-  print "Usage: tester.py <input file> [<command> <serial port>]"
-  exit(1)
+""" Parse it! """
+parser = argparse.ArgumentParser(description="IR Deluxe^2 Test Tool", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--tty', default="/dev/ttyACM0", help="TTY for IR Deluxe^2")
+parser.add_argument('--command', help="Which command to send")
+parser.add_argument('FILE', help="Location to load IR codes (in JSON format)")
+config = parser.parse_args()
 
-jdata = open(sys.argv[1])
+jdata = open(config.FILE)
 data = json.load(jdata)
 
-
-if len(sys.argv) == 2:
-	print "Available commands for device:"
-	for cmd in data:
-		print "  %s" % cmd
+if not config.command:
+  print "Available commands in %s:" % config.FILE
+  for cmd in data:
+    print "  %s" % cmd
 else:
-	if sys.argv[2] not in data:
-		print "ERROR: %s is not a recognized command" % sys.argv[2]
-		exit(1)
+  if config.command not in data:
+    print "ERROR: %s is not a recognized command" % config.command
+    sys.exit(1)
 
-	ir = IRInterface(sys.argv[3])
-	if not ir.init():
-	  print "ERR: Unable to initialize IR interface on %s" % sys.argv[3]
-	  exit(1)
+  sys.stdout.write("Initializing IR Deluxe^2...")
+  sys.stdout.flush()
+  ir = IRInterface(config.tty)
+  if not ir.init():
+    print "ERR: Unable to initialize IR interface on %s" % config.tty
+    sys.exit(1)
 
-	if ir.writeIR(data[sys.argv[2]]):
-		print "INFO: Command sent successfully"
-	else:
-		print "ERR: Was unable to send command"
+  ir.writeIR(data[config.command])
+  result = ir.readIR(True)
+  if result["commandResult"] is "0":
+    print "INFO: Command sent successfully"
+  else:
+    print "ERR: Was unable to send command: " + repr(result)

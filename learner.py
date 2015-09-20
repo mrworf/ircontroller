ba8d8b086f6b1dc6506b0dfa28d5d01d3905b44f
Henric Andersson <henric@sensenet.nu>
2015-09-19 20:26:45 -0700

    learner.py can now convert Pronto HEX codes to IR Deluxe format

--- learner.py
+++ learner.py
@@ -18,13 +18,52 @@ parser.add_argument('--raw', action="store_true", default=False, help="Store all
 parser.add_argument('--convert', action="store_true", default=False, help="Interpret existing codes")
 parser.add_argument('--remove', action="store_true", default=False, help="Remove any IR code which cannot be interpreted")
 parser.add_argument('--learn', action="store_true", default=False, help="Start learning mode")
+parser.add_argument('--pronto', action="store_true", default=False, help="Learning should be done using hex codes")
+
 config = parser.parse_args()
 
 logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(filename)s@%(lineno)d - %(levelname)s - %(message)s')
 
+def parsePronto(raw):
+  # Split into parts
+  parts = raw.split()
+  plearn = None
+  pcarrier = None
+  plead = None
+  pmain = None
+
+  lead = 0
+  main = 0
+  data = []
+
+  for p in parts:
+    p = "%04.4s" % p
+    if plearn is None:
+      if p == "0000":
+        plearn = p
+      else:
+        return None
+    elif pcarrier is None:
+      punit = int(round(int(p, 16) * 0.241246))
+      pcarrier = int(round(1000000/punit))
+    elif plead is None:
+      plead = int(p, 16)*2
+    elif pmain is None:
+      pmain = int(p, 16)*2
+    elif lead < plead:
+      lead += 1
+      data.append(int(round(int(p, 16)*punit)))
+    elif main < pmain:
+      main += 1
+      data.append(int(round(int(p, 16)*punit)))
+  if len(data) % 2:
+    print "ERROR: Not even pairs"
+    return None
+  return {"carrierFreq": pcarrier, "rawTransmit": data}
+
 changed = False
 ir = IRInterface(config.tty)
-if config.learn:
+if config.learn and not config.pronto:
   sys.stdout.write("Initializing IR Deluxe^2...")
   sys.stdout.flush()
 
@@ -96,106 +135,120 @@ if config.learn and config.file:
     if name == "":
       break
 
-    input = []
-    count = 0
-    while True:
-      count = count + 1
-      sys.stdout.write("Ready to receive IR sequence")
-      if count > 1 or config.raw:
-        sys.stdout.write(" (Attempt #%d, need minimum 2)" % count)
-      sys.stdout.write(": ")
+    if config.pronto:
+      sys.stdout.write("Pronto IR HEX Code (or enter to end): ")
       sys.stdout.flush()
-      cmd = {"rawTransmit":[]}
-      # Avoid too short
-      while cmd != None and len(cmd["rawTransmit"]) < 10:
-        ir.clearIR()
-        cmd = ir.readIR(True)
-      # Throw an error if we get none
-      if cmd == None:
-        print "Error reading IR command"
-        count = count - 1
-        continue
-
-      # Continue
-      print "OK"
-
-      # See if we can interpret this, if so, skip cumbersome filtering
-      rec = recognize(cmd["rawTransmit"])
-      if rec is not None:
-        print "Detected %s code." % rec["name"].upper()
-        cmd = {rec["name"] + "Send": [rec["address"],rec["command"]]}
+      raw = sys.stdin.readline().strip()
+      if raw == "":
         break
+      cmd = parsePronto(raw)
+      if not config.raw:
+        rec = recognize(cmd["rawTransmit"])
+        if rec is not None:
+          print "It's actually a %s code, changing." % rec["name"].upper()
+          cmd = {rec["name"] + "Send": [rec["address"],rec["command"]]}
 
-      input.append(cmd)
-      if len(input) > 1:
-        # Now we compare
-        length = {}
-        # Group by size
-        for x in input:
-          key = "%d" % len(x["rawTransmit"])
-          if key in length:
-            length[key].append(x)
-          else:
-            length[key] = [x]
-        # Pick the one with highest count
-        candidate = None
-        for x in length:
-          v = length[x]
-          if candidate is None or len(candidate) < len(v):
-            candidate = v
-        if len(candidate) < 2:
+    else:
+      input = []
+      count = 0
+      while True:
+        count = count + 1
+        sys.stdout.write("Ready to receive IR sequence")
+        if count > 1 or config.raw:
+          sys.stdout.write(" (Attempt #%d, need minimum 2)" % count)
+        sys.stdout.write(": ")
+        sys.stdout.flush()
+        cmd = {"rawTransmit":[]}
+        # Avoid too short
+        while cmd != None and len(cmd["rawTransmit"]) < 10:
+          ir.clearIR()
+          cmd = ir.readIR(True)
+        # Throw an error if we get none
+        if cmd == None:
+          print "Error reading IR command"
+          count = count - 1
           continue
-        print "Candidates: %d" % len(candidate)
 
-        # Now, calculate the max delta
-        mindelta = 70000 # IRDeluxe is limited at max uint16
-        totdelta = 0
-        ideal = None
-        deltas = []
-        # Horrible code, but it will find the entry with least delta
-        for z in range(len(candidate)):
-          baseline = candidate[z]
-          for x in range(z+1, len(candidate)):
-            tester = candidate[x]
-            maxdelta = 0
-            for y in range(len(baseline["rawTransmit"])):
-              delta = abs(baseline["rawTransmit"][y] - tester["rawTransmit"][y])
-              if delta > maxdelta:
-                maxdelta = delta
-            deltas.append(maxdelta)
-            if totdelta < maxdelta:
-              totdelta = maxdelta
-            if maxdelta < mindelta:
-              mindelta = maxdelta
-              ideal = tester
-        deltas.sort()
-        # Statistics...
-        print "Maximum Delta: %d" % totdelta
-        print "Minimum Delta: %d" % mindelta
-        print "Average Delta: %d" % (sum(deltas) / len(deltas))
-        print "Median  Delta: %d" % (deltas[len(deltas)/2])
-        #print "Represented by: " + repr(ideal)
+        # Continue
+        print "OK"
 
-        # Ideally, you want less than 5ms, but 25ms is acceptable
-        choice = "n"
-        if mindelta < 5:
-          sys.stdout.write("We have a great candidate (<5ms jitter), do you wish to save? (Y/n/r) ")
-          sys.stdout.flush()
-          choice = sys.stdin.readline().strip().lower()
-          if choice == "":
-            choice = "y"
-        elif mindelta < 25:
-          sys.stdout.write("We have a good candidate (<25ms jitter), do you wish to save? (Y/n/r) ")
-          sys.stdout.flush()
-          choice = sys.stdin.readline().strip().lower()
-          if choice == "":
-            choice = "y"
-        if choice == "y":
+        # See if we can interpret this, if so, skip cumbersome filtering
+        rec = recognize(cmd["rawTransmit"])
+        if rec is not None:
+          print "Detected %s code." % rec["name"].upper()
+          cmd = {rec["name"] + "Send": [rec["address"],rec["command"]]}
           break
-        elif choice == "r":
-          print "Restarting collection of IR codes"
-          count = 0
-          input = []
+
+        input.append(cmd)
+        if len(input) > 1:
+          # Now we compare
+          length = {}
+          # Group by size
+          for x in input:
+            key = "%d" % len(x["rawTransmit"])
+            if key in length:
+              length[key].append(x)
+            else:
+              length[key] = [x]
+          # Pick the one with highest count
+          candidate = None
+          for x in length:
+            v = length[x]
+            if candidate is None or len(candidate) < len(v):
+              candidate = v
+          if len(candidate) < 2:
+            continue
+          print "Candidates: %d" % len(candidate)
+
+          # Now, calculate the max delta
+          mindelta = 70000 # IRDeluxe is limited at max uint16
+          totdelta = 0
+          ideal = None
+          deltas = []
+          # Horrible code, but it will find the entry with least delta
+          for z in range(len(candidate)):
+            baseline = candidate[z]
+            for x in range(z+1, len(candidate)):
+              tester = candidate[x]
+              maxdelta = 0
+              for y in range(len(baseline["rawTransmit"])):
+                delta = abs(baseline["rawTransmit"][y] - tester["rawTransmit"][y])
+                if delta > maxdelta:
+                  maxdelta = delta
+              deltas.append(maxdelta)
+              if totdelta < maxdelta:
+                totdelta = maxdelta
+              if maxdelta < mindelta:
+                mindelta = maxdelta
+                ideal = tester
+          deltas.sort()
+          # Statistics...
+          print "Maximum Delta: %d" % totdelta
+          print "Minimum Delta: %d" % mindelta
+          print "Average Delta: %d" % (sum(deltas) / len(deltas))
+          print "Median  Delta: %d" % (deltas[len(deltas)/2])
+          #print "Represented by: " + repr(ideal)
+
+          # Ideally, you want less than 5ms, but 25ms is acceptable
+          choice = "n"
+          if mindelta < 5:
+            sys.stdout.write("We have a great candidate (<5ms jitter), do you wish to save? (Y/n/r) ")
+            sys.stdout.flush()
+            choice = sys.stdin.readline().strip().lower()
+            if choice == "":
+              choice = "y"
+          elif mindelta < 25:
+            sys.stdout.write("We have a good candidate (<25ms jitter), do you wish to save? (Y/n/r) ")
+            sys.stdout.flush()
+            choice = sys.stdin.readline().strip().lower()
+            if choice == "":
+              choice = "y"
+          if choice == "y":
+            break
+          elif choice == "r":
+            print "Restarting collection of IR codes"
+            count = 0
+            input = []
 
     print "Saved"
     changed = True

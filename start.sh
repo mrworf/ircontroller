5fa477e12bdea4f72dfaa3856894a0474adbfa58
Henric Andersson <henric@sensenet.nu>
2018-06-20 17:33:13 +0000

    Made IR Deluxe detection simpler
    Cannot rely on dmesg to group things. Now grabs first ACM it finds

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Locate the correct ttyACM for IR Deluxe
-TTY=$(dmesg | grep -A1 "IR Deluxe" | grep ACM | tail -n 1 | egrep -oe 'ttyACM[0-9]')
+TTY=$(dmesg | grep ACM | tail -n 1 | egrep -oe 'ttyACM[0-9]')
 if [ $? -eq 0 -a -e /dev/${TTY} ]; then
 	./server.py --tty /dev/${TTY}
 	exit $?

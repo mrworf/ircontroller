a245ec0daa26c516d7ab4c09db49d742c886f75c
Henric Andersson <henric@sensenet.nu>
2018-06-22 20:54:57 +0000

    Better detection and handling of errors

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Locate the correct ttyACM for IR Deluxe
-TTY=$(dmesg | grep ACM | tail -n 1 | egrep -oe 'ttyACM[0-9]')
-if [ $? -eq 0 -a -e /dev/${TTY} ]; then
-	./server.py --tty /dev/${TTY}
-	exit $?
-fi
-echo "No IR Deluxe device detected!"
-exit 255
+while true; do
+	# Locate the correct ttyACM for IR Deluxe
+	TTY=$(dmesg | grep ACM | tail -n 1 | egrep -oe 'ttyACM[0-9]')
+	HID=$(dmesg | grep hidraw | tail -n 1 | egrep -oe 'hidraw[0-9]')
+	if [ $? -eq 0 -a -e /dev/${TTY} ]; then
+		./server.py --tty /dev/${TTY} --debug /dev/${HID}
+		echo "Server terminated"
+	fi
+	echo "No IR Deluxe device detected!"
+	echo "Sleeping 1s and retrying"
+	sleep 1s
+done
 
